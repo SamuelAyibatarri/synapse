7fa1c492411d1253cccb9159d6975fabdf185747
Samuel Ayibatarri <samuelayibatarri.t@gmail.com>
2026-01-03 00:30:06 +0100

    04: authentication

---
+++ src/lib/auth.ts
@@ -0,0 +1,13 @@
+import { prismaAdapter } from 'better-auth/adapters/prisma';
+import { betterAuth } from "better-auth";
+import prisma from "@/lib/db";
+
+export const auth = betterAuth({
+    database: prismaAdapter(prisma, {
+        provider: "postgresql"
+    }),
+    emailAndPassword: {
+        enabled: true,
+        autoSignIn: true,
+    },
+});
