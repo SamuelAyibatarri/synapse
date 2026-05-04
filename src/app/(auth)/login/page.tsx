7f105882e8b30f707411a640fe76ee211f00ffc8
Samuel Ayibatarri <samuelayibatarri.t@gmail.com>
2026-05-04 18:18:36 +0100

    05: Theme & styling

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,12 +2,8 @@ import { LoginForm } from "@/features/auth/components/login-form";
 import { requireUnAuth } from "@/lib/auth-utils";
 
 const Page = async () => {
-    await requireUnAuth();
-    return (
-        <div>
-            <LoginForm />
-        </div>
-    );
+  await requireUnAuth();
+  return <LoginForm />;
 };
 
 export default Page;
