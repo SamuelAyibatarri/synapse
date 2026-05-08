d92cb624fa83e8b6203690c37c1d919e47e4411a
Samuel Ayibatarri <samuelayibatarri.t@gmail.com>
2026-05-08 12:14:00 +0100

    09: Sidebar Layout

--- next.config.ts
+++ next.config.ts
@@ -1,45 +1,83 @@
-import { withSentryConfig } from "@sentry/nextjs";
-import type { NextConfig } from "next";
+// import { withSentryConfig } from "@sentry/nextjs";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+//   async redirects() {
+//     return [
+//       {
+//         source: "/",
+//         destination: "/workflows",
+//         permanent: false,
+//       },
+//     ];
+//   },
+// };
+
+// export default withSentryConfig(undefined, {
+//   // For all available options, see:
+//   // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+
+//   org: "host-technologies",
+
+//   project: "synapse",
+
+//   // Only print logs for uploading source maps in CI
+//   silent: !process.env.CI,
+
+//   // For all available options, see:
+//   // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+//   // Upload a larger set of source maps for prettier stack traces (increases build time)
+//   widenClientFileUpload: true,
+
+//   // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+//   // This can increase your server load as well as your hosting bill.
+//   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+//   // side errors will fail.
+//   tunnelRoute: "/monitoring",
+
+//   webpack: {
+//     // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+//     // See the following for more information:
+//     // https://docs.sentry.io/product/crons/
+//     // https://vercel.com/docs/cron-jobs
+//     automaticVercelMonitors: true,
+
+//     // Tree-shaking options for reducing bundle size
+//     treeshake: {
+//       // Automatically tree-shake Sentry logger statements to reduce bundle size
+//       removeDebugLogging: true,
+//     },
+//   },
+// });
+
+import { withSentryConfig } from "@sentry/nextjs";
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactCompiler: true,
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/workflows",
+        permanent: false,
+      },
+    ];
+  },
 };
 
-export default withSentryConfig(undefined, {
-  // For all available options, see:
-  // https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
+const sentryOptions = {
   org: "host-technologies",
-
   project: "synapse",
-
-  // Only print logs for uploading source maps in CI
   silent: !process.env.CI,
-
-  // For all available options, see:
-  // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-  // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
-
-  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-  // This can increase your server load as well as your hosting bill.
-  // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-  // side errors will fail.
   tunnelRoute: "/monitoring",
+  hideSourceMaps: true,
+  disableLogger: true,
+  automaticVercelMonitors: true,
+};
 
-  webpack: {
-    // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-    // See the following for more information:
-    // https://docs.sentry.io/product/crons/
-    // https://vercel.com/docs/cron-jobs
-    automaticVercelMonitors: true,
-
-    // Tree-shaking options for reducing bundle size
-    treeshake: {
-      // Automatically tree-shake Sentry logger statements to reduce bundle size
-      removeDebugLogging: true,
-    },
-  },
-});
+export default withSentryConfig(nextConfig, sentryOptions);
---
+++ src/app/(dashboard)/(rest)/credentials/page.tsx
@@ -0,0 +1,8 @@
+import { requireAuth } from "@/lib/auth-utils";
+
+const Page = async () => {
+  await requireAuth();
+  return <p>Credentials page</p>;
+};
+
+export default Page;
